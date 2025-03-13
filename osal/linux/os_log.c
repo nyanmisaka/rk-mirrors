0ebd578486f35f702dde7bec61cf2f76bfa2d5d6
nyanmisaka <nst799610810@gmail.com>
2025-03-14 00:54:30 +0800

    Revert "fix[os_log]: Modify default log option for linux"
    This reverts commit 4ed4105a3c4502d9e63fbe8c6c138c8356329958.

--- osal/linux/os_log.c
+++ osal/linux/os_log.c
@@ -19,17 +19,12 @@
 
 void syslog_wrapper_init()
 {
-    int option = LOG_PID;
-    RK_U32 syslog_perror = 1;
-    RK_U32 syslog_cons = 0;
+    int option = LOG_PID | LOG_CONS;
+    RK_U32 syslog_perror = 0;
 
-    os_get_env_u32("mpp_syslog_perror", &syslog_perror, 1);
+    os_get_env_u32("mpp_syslog_perror", &syslog_perror, 0);
     if (syslog_perror)
         option |= LOG_PERROR;
-
-    os_get_env_u32("mpp_syslog_cons", &syslog_cons, 0);
-    if (syslog_cons)
-        option |= LOG_CONS;
 
     openlog("mpp", option, LOG_USER);
 }
